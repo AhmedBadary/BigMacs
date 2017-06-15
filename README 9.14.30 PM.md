bdf1a46bf93a81c2f5f557277b905d419f176fe7
AhmedBadary <badary.ahmad@gmail.com>
2017-06-14 21:14:49 -0700

    BigMacs chrome-Extension

---
+++ README 9.14.30 PM.md
@@ -0,0 +1,12 @@
+BigMacs
+=============
+
+Know the true worth of things with this amazingly stupid Chrome extension!
+
+<img src="images/bigmacs2.png" />
+<img src="images/bigmacs1.png" />
+
+Acknowledgements
+=============
+
+BigMacs is a modified clone of Caaaarbs, an equally amazingly-stupid extension: https://github.com/laurenorsini/caaaaarbs 
---
+++ script 9.14.30 PM.js
@@ -0,0 +1,49 @@
+walk(document.body);
+
+function walk(node) {
+    // I stole this function from here:
+    // http://is.gd/mwZp7E
+
+    var child, next;
+
+    switch (node.nodeType) {
+        case 1:
+        case 9:
+        case 11:
+            child = node.firstChild;
+            while (child) {
+                next = child.nextSibling;
+                walk(child);
+                child = next;
+            }
+            break;
+
+        case 3:
+            handleText(node);
+            break;
+    }
+}
+
+//credit goes to Steven Frank of Cloud to Butt for handleText Function
+
+function handleText(textNode) {
+    var search_query = /\$\d*\.?\d*( [a-z]?[A-Z]?illion)?/g;
+
+    function replacer(match, p1, offset, string) {
+        var numerals = parseFloat(match.substring(1));
+        if (match.indexOf("illion") > -1) { //if it contains 'illion'
+            numerals = parseFloat(match.substring(1, (match.length - 8))); //Only take the numbers
+        }
+        if (isNaN(numerals)) {
+            return match;
+        }
+        var rounded_numerals = Math.round((numerals / 4) * 100) / 100; // divide by 6.25 and round to 2 decimal places
+        var amount_str = rounded_numerals.toString();
+        if (match.indexOf("Million") > -1) { amount_str = amount_str + " Million"; }
+        if (match.indexOf("million") > -1) { amount_str = amount_str + " Million"; }
+        if (match.indexOf("Billion") > -1) { amount_str = amount_str + " Billion"; }
+        if (match.indexOf("billion") > -1) { amount_str = amount_str + " Billion"; }
+        return amount_str + " Big Macs";
+    }
+    textNode.nodeValue = textNode.nodeValue.replace(search_query, replacer);
+}
